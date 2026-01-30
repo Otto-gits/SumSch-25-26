28be60a341337f2d229bfd8f6ac35018c0a42217
Otto-gits <ottohedger@gmail.com>
2026-01-30 14:05:02 +1030

    Going to run again on the cluster

--- MEA/code/runfile_02.py
+++ MEA/code/runfile_02.py
@@ -1,8 +1,8 @@
 from MEA import MEA_K_plus_1
 
 folder = '../instances/n500w1k10'
-mutation_rates = [0.9]
-k_values = [3, 10]
+mutation_rates = [0.5, 0.9]
+k_values = [10]
 
 print(f"{folder} MEA Results:")
 
@@ -11,7 +11,7 @@ for mut in mutation_rates:
     for j in k_values:
         MEA_evals_per_run = []
 
-        for i in range(5):
+        for i in range(10):
             budget = 10**15
             population_pea, numEvals = MEA_K_plus_1(folder, j, budget, mut)
             MEA_evals_per_run.append(numEvals)
