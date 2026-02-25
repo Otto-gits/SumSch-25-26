1d0879104c7d9517099541b48a2ca2bc66c37cd0
Otto-gits <ottohedger@gmail.com>
2026-02-25 19:37:21 +1030

    gonna run the 50 instance and see what happens

---
+++ MEA/code/runfile_P3.py
@@ -0,0 +1,10 @@
+from PEA import run_PEA_complex
+
+k = [10] 
+for folder in ['../instances/n280']:
+    print(f"{folder} MEA Results:")
+    for j in k:
+        print(f"\n=== k: {j} ===")
+        for i in range(30):
+            budget = 10**6
+            population_pea, numEvals = run_PEA_complex(folder, (budget*j), j)
