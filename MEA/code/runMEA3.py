b44ce7574a89091b587b9bf4a74cd83e051a61e6
Otto-gits <ottohedger@gmail.com>
2026-02-27 18:52:03 +1030

    Final runs nessacary

--- MEA/code/runMEA3.py
+++ MEA/code/runMEA3.py
@@ -1,13 +1,13 @@
 from MEA import run_MEA2
 
 MEA_evals_per_run = []
-k = [5] 
-for folder in ['../instances/2n500w1k10']:
+k = [10] 
+for folder in ['../instances/n500w1k10']:
     print(f"{folder} MEA Results:")
     for j in k:
         print(f"\n=== k: {j} ===")
         MEA_evals_per_run = [] 
-        for i in range(30):
+        for i in range(11):
             budget = 1000000000000000
             population_pea, numEvals = run_MEA2(folder, budget, j)
             MEA_evals_per_run.append(numEvals)
--- MEA/code/runPEA3.py
+++ MEA/code/runPEA3.py
@@ -1,13 +1,13 @@
 from PEA import run_PEA
 
 PEA_evals_per_run = []
-k = [5] 
+k = [10] 
 for folder in ['../instances/n500w1k10']:
     print(f"{folder} PEA Results:")
     for j in k:
         print(f"\n=== k: {j} ===")
         PEA_evals_per_run = [] 
-        for i in range(30):
+        for i in range(5):
             budget = 1000000000000000
             population_pea, numEvals = run_PEA(folder, budget, j)
             PEA_evals_per_run.append(numEvals)
